f7f9cda5431837b5e6f260a037f0556c3dca7fd4
HawookJ <saroball3@naver.com>
2022-01-16 21:48:05 +0900

    임시추가

--- project2_test/src/main/java/com/spring/project2_test/member/controller/MemberControllerImpl.java
+++ project2_test/src/main/java/com/spring/project2_test/member/controller/MemberControllerImpl.java
@@ -1,5 +1,35 @@
 package com.spring.project2_test.member.controller;
 
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.spring.project2_test.member.vo.MemberVO;
+
 public class MemberControllerImpl implements MemberController{
+
+	@Override
+	public ModelAndView login(Map<String, String> loginMap, HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ModelAndView logout(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ResponseEntity addMember(MemberVO member, HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
