8b0b599808824dbed961aa1d60b9cee601f4e765
HawookJ <saroball3@naver.com>
2022-01-25 02:27:12 +0900

    form,main, RequestMapping 내일 추가

--- project2_test/src/main/java/com/spring/project2_test/member/controller/MemberController.java
+++ project2_test/src/main/java/com/spring/project2_test/member/controller/MemberController.java
@@ -5,9 +5,9 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestParam;
+
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -19,6 +19,5 @@ public interface MemberController {
             RedirectAttributes rAttr,
             HttpServletRequest request, HttpServletResponse response) throws Exception;
 	public ModelAndView logout(HttpServletRequest request, HttpServletResponse response) throws Exception;
-	public ResponseEntity  addMember(@ModelAttribute("member") MemberVO member,
-            HttpServletRequest request, HttpServletResponse response) throws Exception;
+	public ModelAndView addMember(@ModelAttribute("info") MemberVO memberVO,HttpServletRequest request, HttpServletResponse response) throws Exception;
 }
