c45f3c2d81c0bc4b3ec20d9ae5f3f794bc31a72a
HawookJ <saroball3@naver.com>
2022-01-25 00:05:05 +0900

    member controller 만드는중

--- project2_test/src/main/java/com/spring/project2_test/member/service/MemberService.java
+++ project2_test/src/main/java/com/spring/project2_test/member/service/MemberService.java
@@ -1,5 +1,14 @@
 package com.spring.project2_test.member.service;
 
-public interface MemberService {
+import org.springframework.dao.DataAccessException;
 
+import com.spring.project2_test.member.vo.MemberVO;
+
+
+
+
+
+public interface MemberService {
+	 public int addMember(MemberVO memberVO) throws DataAccessException;
+	 public MemberVO login(MemberVO memberVO) throws Exception;
 }
