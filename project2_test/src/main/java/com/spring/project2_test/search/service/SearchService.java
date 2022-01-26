253c40228b1a3cb5ac4e45076b15bf67cda30112
HawookJ <saroball3@naver.com>
2022-01-26 14:02:36 +0900

    search 1/5

--- project2_test/src/main/java/com/spring/project2_test/search/service/SearchService.java
+++ project2_test/src/main/java/com/spring/project2_test/search/service/SearchService.java
@@ -1,5 +1,16 @@
 package com.spring.project2_test.search.service;
 
-public interface SearchService {
+import java.util.List;
 
+import com.spring.project2_test.search.vo.SearchVO;
+
+
+
+public interface SearchService {
+	public List<SearchVO> searchRest(String searchWord) throws Exception;
+
+	public List keywordSearch(String keyword);
+
+
+	
 }
